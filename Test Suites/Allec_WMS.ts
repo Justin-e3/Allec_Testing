70ea7a1a15cb23528c44c6fa361f0e65b47a1dd1
justi <justi@Justins-PC>
2022-11-03 23:16:34 -0400

    Log In Test Case

---
+++ Test Suites/Allec_WMS.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Web UI Testing</description>
+   <name>Allec_WMS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a0cf7e89-e6fd-481e-ae76-290e2ac08a0d</testSuiteGuid>
+</TestSuiteEntity>
